26db164eda05e3adf4a1c366cdf17d648899faa4
Julien <julien.berthoumieux@epitech.eu>
2023-01-10 22:50:10 +0100

    wip

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -1,51 +1,76 @@
 import LsdBorder from '../lsd_border';
 import styles from './Contact.module.scss';
+import { useState } from 'react';
 
 export default function Contact() {
+  const [status, setStatus] = useState({
+    submitted: false,
+    error: false,
+    name: '',
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(event.target);
 
-    console.log(data.get('name'));
-    console.log(data.get('email'));
-    console.log(data.get('message'));
+    const data = new FormData(e.target);
+    const name = data.get('name');
 
     fetch(
       'https://public.herotofu.com/v1/7c39c920-9126-11ed-a003-6f0b76086b1c',
-      { method: 'POST', body: data }
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }
     )
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+      .then(() => {
+        setStatus({ submitted: true, error: false, name });
+      })
+      .catch(() => setStatus({ submitted: false, error: true }));
   };
 
   return (
     <div className={styles.contact}>
       <LsdBorder>
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            name='name'
-            placeholder='Nom'
-            className={styles.input}
-          />
-          <input
-            type='email'
-            name='email'
-            placeholder='Email'
-            className={styles.input}
-          />
+        {status.submitted || status.error ? (
+          <Response status={status} />
+        ) : (
+          <form
+            onSubmit={handleSubmit}
+            action='https://public.herotofu.com/v1/7c39c920-9126-11ed-a003-6f0b76086b1c'
+            method='POST'
+            target='_blank'
+          >
+            <input type='text' name='name' placeholder='Nom' required />
+            <input type='email' name='email' placeholder='Email' required />
 
-          <textarea
-            name='message'
-            placeholder='Message'
-            className={styles.textarea}
-          />
+            <textarea name='message' placeholder='Message' required />
 
-          <button type='submit' className={styles.button}>
-            Envoyer
-          </button>
-        </form>
+            <button type='submit' className={styles.button}>
+              Envoyer
+            </button>
+          </form>
+        )}
       </LsdBorder>
     </div>
   );
 }
+
+const Response = ({ status }) => {
+  return (
+    <div className={styles.response}>
+      {status.submitted ? (
+        <p className={styles.success}>
+          Merci {status.name}, je vous recontacte très vite !
+        </p>
+      ) : (
+        <p className={styles.error}>
+          Oupsi, une erreur est survenue. Je m'occupe de ça rapidement !
+        </p>
+      )}
+    </div>
+  );
+};
--- components/lsd_border/index.js
+++ components/lsd_border/index.js
@@ -1,22 +1,25 @@
 import { useEffect, useRef } from 'react';
+
 import styles from './LsdBorder.module.scss';
 
 const getDegree = (element, event) => {
   const { left, top } = element.getBoundingClientRect();
   const x = event.clientX - left;
   const y = event.clientY - top;
-  const degree = Math.atan2(y, x) * 180 / Math.PI + 180;
+  const degree = (Math.atan2(y, x) * 180) / Math.PI + 180;
 
   return degree;
-}
+};
 
 const useGradientBorder = () => {
   const ref = useRef();
 
   useEffect(() => {
     const handleMouseMove = (event) => {
-      const degree = getDegree(ref.current, event);
-      ref.current.style.setProperty('--gradient-rotation', `${degree}deg`);
+      if (ref.current) {
+        const degree = getDegree(ref.current, event);
+        ref.current.style.setProperty('--gradient-rotation', `${degree}deg`);
+      }
     };
 
     document.addEventListener('mousemove', handleMouseMove);
@@ -27,7 +30,7 @@ const useGradientBorder = () => {
   }, []);
 
   return ref;
-}
+};
 
 export default function LsdBorder({ children }) {
   const ref = useGradientBorder();
@@ -36,5 +39,5 @@ export default function LsdBorder({ children }) {
     <div ref={ref} className={styles.lsd_border}>
       {children}
     </div>
-  )
+  );
 }
