768f9e8cd29ac1579b53cd14d7e32eb4fa2d429d
Julien <julienberthoumieux.dev@gmail.com>
2023-01-12 15:45:09 +0100

    add website title

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="description" content="Site internet de Julien Berthoumieux, développeur web." />
+        <title>Julien Berthoumieux</title>
       </Head>
       <body>
         <Main />
