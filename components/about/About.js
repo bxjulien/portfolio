3e9cc60727f0a76fcd657b1a8822ae7fc4efeed3
Julien <julienberthoumieux.dev@gmail.com>
2023-01-27 14:11:50 +0100

    bio rework

--- components/about/About.js
+++ components/about/About.js
@@ -8,18 +8,7 @@ export default function About() {
 
   return (
     <section className={styles.about}>
-      <p ref={ref} className={isVisible ? styles.visible : ''}>
-        {t('bio_1')}<span>{t('bio_2')}</span>{t('bio_3')}
-        <br />
-        {t('bio_4')}
-        <br />
-        {t('bio_5')}<span>{t('bio_6')}</span>{t('bio_7')}
-        {t('bio_8')}<span>{t('bio_9')}</span>{t('bio_10')}<span>{t('bio_11')}</span>.
-        <br />
-        {t('bio_12')}<span>{t('bio_13')}</span>{t('bio_14')}<span>{t('bio_15')}</span>{t('bio_16')}
-        <br />
-        {t('bio_17')}👟
-      </p>
+      <p ref={ref} className={isVisible ? styles.visible : ''} dangerouslySetInnerHTML={{ __html: t('bio') }}></p>
     </section>
   );
 }
