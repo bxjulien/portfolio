dd26f4c3c47467ae2e9bed20b8a59b588a4dfe73
Julien <julien.berthoumieux@epitech.eu>
2023-01-28 17:17:22 +0100

    update cv

--- pages/cv.js
+++ pages/cv.js
@@ -1,22 +1,27 @@
-import Header from '@/components/header/Header';
-import useTranslation from 'next-translate/useTranslation';
-import styles from '../styles/CV.module.scss';
-import { FaUser } from 'react-icons/fa';
-import { BsFillBriefcaseFill, BsPiggyBank, BsCalendarMinus } from 'react-icons/bs';
-import { IoSchool } from 'react-icons/io5';
-import { FaReact, FaNodeJs } from 'react-icons/fa';
-import { HiPhone } from 'react-icons/hi';
-import { SiJavascript } from 'react-icons/si';
-import { TbBrandFirebase, TbBuilding } from 'react-icons/tb';
-import { TbBrandNextjs } from 'react-icons/tb';
-import { BiCameraMovie } from 'react-icons/bi';
-import { MdCake } from 'react-icons/md';
-import { RiTeamFill, RiFocusLine } from 'react-icons/ri';
-import { ImBubbles } from 'react-icons/im';
-import { MdAutoFixHigh } from 'react-icons/md';
+import {
+  BsCalendarMinus,
+  BsFillBriefcaseFill,
+  BsPiggyBank,
+} from 'react-icons/bs';
+import { FaNodeJs, FaReact } from 'react-icons/fa';
 import { GiRunningNinja, GiRunningShoe } from 'react-icons/gi';
-import { SiCypress } from 'react-icons/si';
+import { RiFocusLine, RiTeamFill } from 'react-icons/ri';
+import { TbBrandFirebase, TbBuilding } from 'react-icons/tb';
+
 import Badge from '@/components/badge/Badge';
+import { BiCameraMovie } from 'react-icons/bi';
+import { FaUser } from 'react-icons/fa';
+import Header from '@/components/header/Header';
+import { HiPhone } from 'react-icons/hi';
+import { ImBubbles } from 'react-icons/im';
+import { IoSchool } from 'react-icons/io5';
+import { MdAutoFixHigh } from 'react-icons/md';
+import { MdCake } from 'react-icons/md';
+import { SiCypress } from 'react-icons/si';
+import { SiJavascript } from 'react-icons/si';
+import { TbBrandNextjs } from 'react-icons/tb';
+import styles from '../styles/CV.module.scss';
+import useTranslation from 'next-translate/useTranslation';
 
 export default function CV() {
   const { t } = useTranslation('common');
@@ -29,29 +34,35 @@ export default function CV() {
         <div className={styles.content}>
           <div className={styles.main}>
             <section className={styles.profile}>
-
               <div className={styles.title}>
                 <div className={styles.icon}>
-                  <FaUser size="22" />
+                  <FaUser size='22' />
                 </div>
                 <h3>Profil</h3>
               </div>
 
               <div className={styles.info}>
                 <p>
-                  Jeune développeur passionné, je suis à la recherche d'une entreprise pour continuer à développer mes compétences et contribuer au succès de projets ambitieux.
+                  J’aime designer, concevoir et développer des solutions
+                  performantes et modernes tout en apportant un soin particulier
+                  à l’expérience utilisateur.
                   <br />
-                  Mon entrain et mon esprit d'équipe font de moi un candidat idéal pour intégrer une équipe et l'aider à atteindre ses objectifs.
+                  Je me forme constamment et essai de produire un code de
+                  qualité respectant au maximum les bonnes pratiques.
+                  <br />
+                  Je suis à la recherche d'une entreprise pour continuer à
+                  développer mes compétences et contribuer au succès de projets
+                  ambitieux. Mon entrain et mon esprit d'équipe font de moi un
+                  candidat idéal pour intégrer une équipe et l'aider à atteindre
+                  ses objectifs.
                 </p>
               </div>
-
             </section>
 
             <section className={styles.experiences}>
-
               <div className={styles.title}>
                 <div className={styles.icon}>
-                  <BsFillBriefcaseFill size="22" />
+                  <BsFillBriefcaseFill size='22' />
                 </div>
                 <h3>Experiences professionnelles</h3>
               </div>
@@ -61,11 +72,11 @@ export default function CV() {
                   <div>
                     <h4>Développeur Web Fullstack, Ubigreen, Alternance</h4>
                     <span>
-                      <Badge variant="classic">
+                      <Badge variant='classic'>
                         <BsCalendarMinus />
-                        Mars 2020 - Présent
+                        mars 2020 - présent · ~3ans
                       </Badge>
-                      <Badge variant="classic">
+                      <Badge variant='classic'>
                         <TbBuilding />
                         Proptech
                       </Badge>
@@ -73,11 +84,9 @@ export default function CV() {
                   </div>
 
                   <p>
-                    J'interviens sur les applications web et mobile de mon entreprise en travaillant en étroite collaboration avec l'ensemble des équipes.
-                  </p>
-
-                  <p>
-                    Activitées principales :
+                    J'interviens sur les applications web et mobile de mon
+                    entreprise en travaillant en étroite collaboration avec
+                    l'ensemble des équipes.
                   </p>
 
                   <ul>
@@ -87,7 +96,6 @@ export default function CV() {
                     <li>Intégration de données</li>
                   </ul>
                 </div>
-
               </div>
 
               <div className={styles.obie}>
@@ -95,11 +103,11 @@ export default function CV() {
                   <div>
                     <h4>Ingénieur Logiciel Web, Obie, Indépendant</h4>
                     <span>
-                      <Badge variant="classic">
+                      <Badge variant='classic'>
                         <BsCalendarMinus />
-                        Juin 2022 - Présent
+                        juin 2022 - présent · ~9 mois
                       </Badge>
-                      <Badge variant="classic">
+                      <Badge variant='classic'>
                         <BsPiggyBank />
                         Finance
                       </Badge>
@@ -107,35 +115,30 @@ export default function CV() {
                   </div>
 
                   <p>
-                    Obie est une application web destinée aux travailleurs ou frontaliers suisses facilitant la récupération de leurs avoirs LPP.
-                    <br />
-                    Je tiens le rôle de lead concepteur développeur et travaille en collaboration avec mon client qui est maitre des règles métiers.
-                  </p>
-
-                  <p>
-                    Les fonctionnalités principales sont :
+                    Je tiens le rôle de concepteur développeur et travaille en
+                    collaboration avec mon client qui est maitre des règles
+                    métiers.
                   </p>
 
                   <ul>
                     <li>Landing page publique présentant le produit</li>
                     <li>Formulaire avancé de recherche d'avoirs LPP</li>
-                    <li>Dashboard administrateur sécurisé proposant une gestion totale des données</li>
+                    <li>
+                      Dashboard administrateur sécurisé proposant une gestion
+                      totale des données
+                    </li>
                     <li>Emailing sur mesure</li>
                     <li>Génération de PDF sur mesure</li>
                     <li>Gestion intégrale multi-langues</li>
                   </ul>
-
-
                 </div>
               </div>
-
             </section>
 
             <section className={styles.educations}>
-
               <div className={styles.title}>
                 <div className={styles.icon}>
-                  <IoSchool size="22" />
+                  <IoSchool size='22' />
                 </div>
                 <h3>Etudes</h3>
               </div>
@@ -154,7 +157,7 @@ export default function CV() {
                     <h4>Epitech, Licence</h4>
                     <span className={styles.dates}>2020 - 2021</span>
                   </div>
-                  <p>Concepteur Développeur d'Application</p>
+                  <p>Concepteur Développeur d'Applications</p>
                 </div>
 
                 <div>
@@ -165,7 +168,6 @@ export default function CV() {
                   <p>Développeur Web et Mobile</p>
                 </div>
               </div>
-
             </section>
           </div>
 
@@ -185,10 +187,10 @@ export default function CV() {
             </section>
 
             <section>
-              <h3>Technologies</h3>
+              <h3>Hard Skills</h3>
 
               <Badge variant='classic'>
-                <SiJavascript size={22} color="#FFCB2B" />
+                <SiJavascript size={22} color='#FFCB2B' />
                 JavaScript
               </Badge>
 
@@ -216,7 +218,6 @@ export default function CV() {
                 <SiCypress size={22} color='black' />
                 Cypress
               </Badge>
-
             </section>
 
             <section>
@@ -245,7 +246,6 @@ export default function CV() {
                 <MdAutoFixHigh />
                 Créatif
               </Badge>
-
             </section>
 
             <section>
@@ -260,12 +260,10 @@ export default function CV() {
                 <BiCameraMovie />
                 Cinéma
               </Badge>
-
             </section>
           </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
