9c9fecc326797c51f48668682522a7d1cf02e7fa
Julien B <jberthoumieux@ubigreen.com>
2023-03-02 17:13:06 +0100

    add second font

--- components/title/Title.js
+++ components/title/Title.js
@@ -1,50 +1,46 @@
-import useVisible from 'hooks/useVisible';
-import styles from './Title.module.scss';
+import useVisible from "hooks/useVisible";
+import styles from "./Title.module.scss";
 
 export default function Title({
   className,
   id,
   children,
   variant,
-  tag = 'h1',
+  tag = "h1",
   subtitle,
 }) {
   const [ref, isVisible] = useVisible();
 
   return (
     <div
-      className={`${className ? className : ''} ${styles.title} ${variant ? styles[variant] : ''
-        }`}
-      id={id || ''}
+      className={`${className ? className : ""} ${styles.title} ${
+        variant ? styles[variant] : ""
+      }`}
+      id={id || ""}
     >
       <div className={styles.title_inner}>
         <Tag tag={tag} children={children} />
       </div>
-      {subtitle && <span className={`${styles.title_subtitle} ${isVisible ? styles.visible : ''}`} ref={ref}>{subtitle}</span>}
+      {subtitle && (
+        <span
+          className={`${styles.title_subtitle} ${
+            isVisible ? styles.visible : ""
+          }`}
+          ref={ref}
+        >
+          {subtitle}
+        </span>
+      )}
     </div>
   );
 }
 
 const Tag = ({ tag, children }) => {
-  if (tag === 'h1') {
-    return (
-      <h1 className={styles.title_text} data-title={children}>
-        {children}
-      </h1>
-    );
-  }
-  if (tag === 'h2') {
-    return (
-      <h2 className={styles.title_text} data-title={children}>
-        {children}
-      </h2>
-    );
-  }
-  if (tag === 'h3') {
-    return (
-      <h3 className={styles.title_text} data-title={children}>
-        {children}
-      </h3>
-    );
-  }
+  const Tag = tag.toLowerCase();
+
+  return (
+    <Tag className={styles.title_text} data-title={children}>
+      {children}
+    </Tag>
+  );
 };
