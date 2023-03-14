4cee78d6b3b7aea396420b5c9e33170efa8cb2bb
Julien <julien.berthoumieux@epitech.eu>
2023-03-14 21:43:23 +0100

    fix dead external link

--- components/works/Works.js
+++ components/works/Works.js
@@ -1,26 +1,33 @@
-import Badge from "../badge/Badge";
-import Card from "../card/Card";
-import { FaNodeJs } from "react-icons/fa";
-import { FaReact } from "react-icons/fa";
-import { TbBrandFirebase } from "react-icons/tb";
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiCypress, SiGraphql, SiPrisma, SiTypescript, SiVite } from "react-icons/si";
-import styles from "./Works.module.scss";
-import Image from "next/image";
-import Link from "next/link";
+import {
+  SiCypress,
+  SiGraphql,
+  SiPrisma,
+  SiTypescript,
+  SiVite,
+} from 'react-icons/si';
+
+import Badge from '../badge/Badge';
+import Card from '../card/Card';
+import { FaNodeJs } from 'react-icons/fa';
+import { FaReact } from 'react-icons/fa';
+import Image from 'next/image';
+import Link from 'next/link';
+import { TbBrandFirebase } from 'react-icons/tb';
+import { TbBrandNextjs } from 'react-icons/tb';
+import styles from './Works.module.scss';
 
 export default function Works({ t }) {
   return (
     <section id={styles.works}>
       <div className={styles.work}>
-        <Card variant="no_padding">
+        <Card variant='no_padding'>
           <div className={styles.image}>
             <div className={styles.overlay}>
               <small>Release 2023</small>
             </div>
             <Image
-              src={"/obie2.png"}
-              alt="obie.ch"
+              src={'/obie2.png'}
+              alt='obie.ch'
               width={800}
               height={500}
               quality={100}
@@ -30,71 +37,71 @@ export default function Works({ t }) {
           <div className={styles.work_content}>
             <div className={styles.title}>
               <h3>Obie.ch</h3>
-              <small>{t("obie_dates")}</small>
+              <small>{t('obie_dates')}</small>
             </div>
 
             <div className={styles.description}>
               <p>
-                {t("obie_description_1")}
+                {t('obie_description_1')}
                 <br />
                 <br />
-                {t("obie_description_2")}
+                {t('obie_description_2')}
                 <br />
                 <br />
-                {t("obie_description_3")}
+                {t('obie_description_3')}
                 <br />
                 <br />
               </p>
               <ul>
                 <li>
-                  <strong>{t("obie_feature_1_0")}</strong>{" "}
-                  {t("obie_feature_1_1")}
+                  <strong>{t('obie_feature_1_0')}</strong>{' '}
+                  {t('obie_feature_1_1')}
                 </li>
                 <li>
-                  <strong>{t("obie_feature_2_0")}</strong>{" "}
-                  {t("obie_feature_2_1")}
+                  <strong>{t('obie_feature_2_0')}</strong>{' '}
+                  {t('obie_feature_2_1')}
                 </li>
                 <li>
-                  <strong>{t("obie_feature_3_0")}</strong>{" "}
-                  {t("obie_feature_3_1")}
+                  <strong>{t('obie_feature_3_0')}</strong>{' '}
+                  {t('obie_feature_3_1')}
                 </li>
                 <li>
-                  <strong>{t("obie_feature_4_0")}</strong>{" "}
-                  {t("obie_feature_4_1")}
+                  <strong>{t('obie_feature_4_0')}</strong>{' '}
+                  {t('obie_feature_4_1')}
                 </li>
                 <li>
-                  <strong>{t("obie_feature_5_0")}</strong>{" "}
-                  {t("obie_feature_4_1")}
+                  <strong>{t('obie_feature_5_0')}</strong>{' '}
+                  {t('obie_feature_4_1')}
                 </li>
                 <li>
-                  <strong>{t("obie_feature_6_0")}</strong>
+                  <strong>{t('obie_feature_6_0')}</strong>
                 </li>
               </ul>
             </div>
 
             <div className={styles.footer}>
               <div className={styles.badges}>
-                <Badge variant="default">
+                <Badge variant='default'>
                   <TbBrandNextjs size={27} />
                   Next.JS
                 </Badge>
 
-                <Badge variant="default">
-                  <FaReact size={25} color="#61DAFB" />
+                <Badge variant='default'>
+                  <FaReact size={25} color='#61DAFB' />
                   React
                 </Badge>
 
-                <Badge variant="default">
-                  <FaNodeJs size={25} color="#509941" />
+                <Badge variant='default'>
+                  <FaNodeJs size={25} color='#509941' />
                   Node.JS
                 </Badge>
 
-                <Badge variant="default">
-                  <TbBrandFirebase size={25} color="#FFCB2B" />
+                <Badge variant='default'>
+                  <TbBrandFirebase size={25} color='#FFCB2B' />
                   Firebase
                 </Badge>
 
-                <Badge variant="default">
+                <Badge variant='default'>
                   <SiCypress size={22} />
                   Cypress
                 </Badge>
@@ -108,16 +115,16 @@ export default function Works({ t }) {
       </div>
 
       <div className={styles.work}>
-        <Card variant="no_padding">
+        <Card variant='no_padding'>
           <Link
-            href="https://github.com/bxjulien/blabble"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://github.com/bxjulien/blabble'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <div className={styles.image}>
               <Image
-                src={"/blabble.png"}
-                alt="blabble"
+                src={'/blabble.png'}
+                alt='blabble'
                 width={800}
                 height={500}
                 quality={100}
@@ -132,38 +139,43 @@ export default function Works({ t }) {
 
             <div className={styles.description}>
               <p
-                dangerouslySetInnerHTML={{ __html: t("blabble_description") }}
+                dangerouslySetInnerHTML={{
+                  __html: t('blabble_description').replace(
+                    '"’blank’"',
+                    '"_blank"'
+                  ),
+                }}
               ></p>
             </div>
 
             <div className={styles.footer}>
               <div className={styles.badges}>
-                <Badge variant="default">
-                  <SiTypescript size={22} color="#2F74C0" />
+                <Badge variant='default'>
+                  <SiTypescript size={22} color='#2F74C0' />
                   TypeScript
                 </Badge>
 
-                <Badge variant="default">
-                  <FaReact size={25} color="#61DAFB" />
+                <Badge variant='default'>
+                  <FaReact size={25} color='#61DAFB' />
                   React
                 </Badge>
 
-                <Badge variant="default">
-                  <FaNodeJs size={25} color="#509941" />
+                <Badge variant='default'>
+                  <FaNodeJs size={25} color='#509941' />
                   Node.JS
                 </Badge>
 
-                <Badge variant="default">
+                <Badge variant='default'>
                   <SiGraphql size={24} color='#DE33A6' />
                   GraphQL
                 </Badge>
 
-                <Badge variant="default"> 
+                <Badge variant='default'>
                   <SiPrisma size={25} />
                   Prisma
                 </Badge>
 
-                <Badge variant="default">
+                <Badge variant='default'>
                   <SiVite size={25} color='#FFB70F' />
                   Vite
                 </Badge>
@@ -172,7 +184,7 @@ export default function Works({ t }) {
           </div>
           <div className={styles.alert}>
             <small>
-              <a href="https://github.com/bxjulien/blabble" target="_blank">
+              <a href='https://github.com/bxjulien/blabble' target='_blank'>
                 Lien vers le code source
               </a>
             </small>
