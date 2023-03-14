40cb71c5bccd0a7c48e629bcd8f8dec30750f5ff
Julien <julien.berthoumieux@epitech.eu>
2023-03-14 21:51:33 +0100

    fix nextjs _document & _app files warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.scss';
 
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
-  )
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>Julien Berthoumieux</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang='fr'>
       <Head>
-        <link rel="icon" href="/favicon.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
+        <link rel='icon' href='/favicon.svg' />
+
+        <meta charSet='utf-8' />
         <meta
-          name="description"
-          content="Je suis un développeur passionné de 28 ans basé à Toulouse. J’aime designer, concevoir et développer des solutions performantes et modernes tout en apportant un soin particulier à l’expérience utilisateur."
+          name='description'
+          content='Je suis un développeur passionné de 28 ans basé à Toulouse. J’aime designer, concevoir et développer des solutions performantes et modernes tout en apportant un soin particulier à l’expérience utilisateur.'
         />
-        <title>Julien Berthoumieux</title>
       </Head>
       <body>
         <Main />
